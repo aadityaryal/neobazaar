63b24a7cf5a380ae14d3c96a840834e0d1a7713b
Aaditya Aryal <aadityaryal8@gmail.com>
2026-01-02 11:27:00 +0545

    Add login provider

--- lib/features/auth/presentation/view_model/auth_view_model.dart
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -3,6 +3,11 @@ import 'package:neobazaar/features/auth/domain/usecases/login_usecase.dart';
 import 'package:neobazaar/features/auth/domain/usecases/register_usecase.dart';
 import 'package:neobazaar/features/auth/presentation/state/auth_state.dart';
 
+// providier 
+final authViewModelProvider = NotifierProvider<AuthViewModel, AuthState>(
+  () => AuthViewModel(),
+);
+
 class AuthViewModel extends Notifier<AuthState> {
   late final RegisterUsecase _registerUsecase;
   late final LoginUsecase _loginUsecase;
@@ -37,13 +42,40 @@ class AuthViewModel extends Notifier<AuthState> {
       },
       (isRegistered) {
         if (isRegistered) {
-          state = state.copyWith(status: AuthStatus.unauthenticated);
+          state = state.copyWith(status: AuthStatus.registered);
         } else {
           state = state.copyWith(
             status: AuthStatus.error,
             errorMessage: "Registration failed",
           );
         }
+      },
+    );
+  }
+
+  // login 
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = LoginUsecaseParams(
+      email: email,
+      password: password,
+    );
+    final result = await _loginUsecase(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
       },
     );
   }

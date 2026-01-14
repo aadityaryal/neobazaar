8c47a24129cc4b69d4ea882b0d8352b383330c37
Aaditya Aryal <aadityaryal8@gmail.com>
2026-01-14 23:29:00 +0545

    Refactor main function to initialize HiveService and SharedPreferences before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:neobazaar/app/app.dart';
+import 'package:neobazaar/core/services/hive/hive_service.dart';
+import 'package:neobazaar/core/services/storage/user_session_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   // use provider scope 
 
-  runApp(const ProviderScope(child: MyApp()));
+  await HiveService().init();
+
+  final sharedPrefs = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [sharedPreferencesProvider.overrideWithValue(sharedPrefs)],
+      child: const MyApp()));
 }
